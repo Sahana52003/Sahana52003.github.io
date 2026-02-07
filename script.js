a7f1f542237bf381199ef93f25fbafbb5a59c496
Sahana52003 <sahana123xworkz@gmail.com>
2026-02-07 19:23:56 +0530

    Remove Typed.js initialization and downloadResume function
    Removed event listener for Typed.js and downloadResume function.

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  new Typed(".text", {
-    strings: ["Frontend Developer", "Full Stack Developer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    backDelay: 1000,
-    loop: true
-  });
-});
 
-function downloadResume() {
-    const link = document.createElement('a');
-    link.href = 'SAHANA-N_Resume.pdf'; // Path to your resume
-    link.download = 'SAHANA-N_Resume.pdf'; // Name for the downloaded file
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
